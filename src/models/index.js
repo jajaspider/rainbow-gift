97920dd5553e72144a1cf6fd9148e33d6db2ec7e
jajaspider <jaja_spider@naver.com>
2022-04-24 02:48:03 +0900

    feat: Change mongoose initailize
    model이 불러질때, mongoose가 초기화 되도록 하였습니다.

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const _ = require('lodash');
+
+let configPath = path.join(process.cwd(), 'config', 'develop.yaml');
+let config = yaml.load(fs.readFileSync(configPath));
+
+let mongoConfig = _.get(config, 'mongo');
+
+mongoose
+    .connect(`mongodb://${mongoConfig['ip']}:${mongoConfig['port']}/${mongoConfig['database']}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Successfully connected to mongodb'))
+    .catch(e => {
+        console.error(e);
+        throw new Error('mongo DB connection fail');
+    });
 
 // const roomSchema = require('./room');
 const userSchema = require('./user');
